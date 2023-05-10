ecf445aa1d94eef7a67570670bf64f8dcd816b09
Paulina Szamraj <paulinaps.93@wp.pl>
2023-05-10 17:21:02 +0200

    added useEffect in import

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const getInitialTasks = () => {
   const tasksFromLocalStorage = localStorage.getItem("tasks");
