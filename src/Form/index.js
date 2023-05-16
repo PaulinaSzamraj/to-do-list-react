2b6cf32734a508aa15700cd193886fe865c7c2cf
Paulina Szamraj <paulinaps.93@wp.pl>
2023-05-16 13:06:18 +0200

    edited onFormSubmit

--- src/Form/index.js
+++ src/Form/index.js
@@ -6,10 +6,14 @@ const Form = ({ addNewTask }) => {
 
   const onFormSubmit = (event) => {
     event.preventDefault();
-    if (newTaskContent === "") {
+
+    const trimmedNewTaskContent = newTaskContent.trim();
+
+    if (trimmedNewTaskContent === "") {
       return;
     }
-    addNewTask(newTaskContent.trim());
+
+    addNewTask(trimmedNewTaskContent);
     setNewTaskContent("");
   };
 
