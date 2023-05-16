3802098b544b264194bf05caf5479257e645a7d2
Paulina Szamraj <paulinaps.93@wp.pl>
2023-05-16 13:06:03 +0200

    edited getInitialTasks

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ import Header from "./Header";
 import Container from "./Container";
 import { useState, useEffect } from "react";
 
-const getInitialTasks = () => {
-  const tasksFromLocalStorage = localStorage.getItem("tasks");
-  return tasksFromLocalStorage ? JSON.parse(tasksFromLocalStorage) : [];
-};
+const getInitialTasks = () => JSON.parse(localStorage.getItem("tasks")) || [];
 
 function App() {
   const [hideDone, setHideDone] = useState(false);
